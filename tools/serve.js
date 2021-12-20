1b8b68dcd689e7c0c3bde250a60584cfa63cbf76
Alexsander Akers <me@a2.io>
2021-12-19 21:25:20 -0500

    Change build.js to serve.js

---
+++ tools/serve.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const esbuild = require("esbuild");
+const fs = require("fs");
+const path = require("path");
+
+if (process.stdin.isTTY) {
+  throw new Error("Expected data from stdin");
+}
+
+const code = fs.readFileSync(process.stdin.fd, "utf8");
+const build = process.argv.indexOf("--build") !== -1;
+const watch = process.argv.indexOf("--watch") !== -1;
+
+const dist = path.join(__dirname, "..", "dist");
+
+/** @type esbuild.BuildOptions */
+const buildOptions = {
+  bundle: true,
+  entryPoints: [path.join(__dirname, "..", "src/index.ts")],
+  define: { code: JSON.stringify(code) },
+  minify: process.env.NODE_ENV === "production",
+  outfile: path.join(dist, "bundle.js"),
+  sourcemap: true,
+  watch: watch,
+};
+
+if (build) {
+  esbuild.build(buildOptions).catch(() => process.exit(1));
+} else {
+  esbuild.serve({ servedir: dist }, buildOptions).then((result) => {
+    console.log(`Serving on port ${result.port}...`);
+  });
+}
